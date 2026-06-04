6c87928e57ceb8ca609f30de87d04d4d144aeb0e
Taurus McLure <taurus@mclure.ru>
2026-06-04 18:14:09 +0300

    Fix backup size: use filesize field (actual file size) instead of virtual disk size

--- ui/detail_panel.py
+++ ui/detail_panel.py
@@ -1536,7 +1536,7 @@ class DetailPanel(QWidget):
             table.setItem(i, 0, QTableWidgetItem(f"VM {b.get('vmid', '')}"))
             table.setItem(i, 1, QTableWidgetItem(b.get("type", "")))
             table.setItem(i, 2, QTableWidgetItem(b.get("format", "")))
-            size = b.get("size", 0) or 0
+            size = b.get("filesize", 0) or b.get("size", 0) or 0
             table.setItem(i, 3, QTableWidgetItem(self._format_volsize(size) if size else "0"))
             ctime = b.get("ctime")
             if ctime:
