22b7a93933d59a6c9a4114f888344b445b420a0e
Taurus McLure <taurus@mclure.ru>
2026-06-10 08:00:25 +0300

    fix: proper setup.py with find_packages, restore %py3_install

--- setup.py
+++ setup.py
@@ -1,2 +1,7 @@
-from setuptools import setup
-setup()
+from setuptools import setup, find_packages
+setup(
+    name="pvecenter",
+    version="1.0.0",
+    packages=find_packages(include=["pve_center", "pve_center.*"]),
+    entry_points={"console_scripts": ["pvecenter=pve_center.main:main"]},
+)
