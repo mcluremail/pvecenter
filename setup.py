44aab453fe4aede0800a55bc6b444f08210f45e1
Taurus McLure <taurus@mclure.ru>
2026-09-03 12:20:43 +0300

    chore: fix ruff lint in setup.py (import order, trailing newline)

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@
 pyproject.toml is authoritative. This exists only because Fedora's
 %py3_build macro calls `setup.py build` directly.
 """
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(packages=find_packages(include=["pve_center*"]))
